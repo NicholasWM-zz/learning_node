3f6aaccd326f4cdd84d995884946ebedec294c7f
NicholasWM <nicholaswesleymazzei@gmail.com>
2020-03-19 21:08:46 -0300

    Estrutura de pastas prontas para implementação dos objetos concretos das bases de dados

---
+++ Multi-DB/src/db/strategies/postgres.js
@@ -0,0 +1,15 @@
+const ICrud = require('./base/interfaces/ICrud')
+
+//Classe concreta que implenta as funções de fato
+class Postgres extends ICrud {
+    constructor (){
+        super()
+    }
+
+    create(item){
+        console.log("O item foi salvo em Postgres");
+        
+    }
+}
+
+module.exports = Postgres
